8db94c50cb265f5882b241980c3eec1fe40152a3
PedroRuizAguilar <cocode2002@gmail.com>
2023-03-07 17:34:41 +0100

    Fix #41 Creada entidad peep

---
+++ src/main/java/acme/entities/Peep.java
@@ -0,0 +1,48 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Peep extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			message;
+
+	@Email
+	protected String			email;
+
+	@URL
+	protected String			link;
+
+	@PastOrPresent
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotBlank
+	@NotNull
+	protected Date				instation_moment;
+
+}
