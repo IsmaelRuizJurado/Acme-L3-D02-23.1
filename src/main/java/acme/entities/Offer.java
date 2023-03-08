614032ed4be3d9c392910d4c47fcb7b5063b6fc6
jescarcon <jesuscardenas2000@gmail.com>
2023-03-08 21:12:12 +0100

    Iniciado de rama

--- src/main/java/acme/entities/Offer.java
+++ src/main/java/acme/entities/Offer.java
@@ -4,7 +4,7 @@ package acme.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -67,7 +67,7 @@ public class Offer extends AbstractEntity {
 
 	@Valid
 	@NotNull
-	@OneToMany
+	@ManyToOne
 	protected Administrator		poster;
 
 }
--- src/main/java/acme/roles/Lecturer.java
+++ src/main/java/acme/roles/Lecturer.java
@@ -1,12 +1,18 @@
 
 package acme.roles;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.entities.course.Course;
 // import acme.entities.course.Course;
 import acme.framework.data.AbstractRole;
 import lombok.Getter;
@@ -19,31 +25,31 @@ public class Lecturer extends AbstractRole {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long	serialVersionUID	= 1L;
+	protected static final long		serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
 	@Length(min = 1, max = 75)
-	protected String			alma_mater;
+	protected String				alma_mater;
 
 	@NotBlank
 	@Length(min = 1, max = 100)
-	protected String			resume;
+	protected String				resume;
 
 	@NotBlank
 	@Length(min = 1, max = 100)
-	protected String			qualifications;
+	protected String				qualifications;
 
 	@NotBlank
 	@URL
-	protected String			link;
+	protected String				link;
 
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-	//	@Valid
-	//	@NotNull
-	//	@OneToMany(mappedBy = "course_lecturer")
-	//	protected Collection<Course>	courses;
+	@Valid
+	@NotNull
+	@OneToMany(mappedBy = "course_lecturer")
+	protected Collection<Course>	courses;
 }
