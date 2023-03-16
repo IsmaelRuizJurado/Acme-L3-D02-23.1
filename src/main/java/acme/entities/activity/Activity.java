31dc65553bed72aceb536b1fa6f59959e097bce6
antoj2k <antlopcub@alum.us.es>
2023-03-16 15:59:18 +0100

    Añadida entidad Activity (para workbook)

---
+++ src/main/java/acme/entities/activity/Activity.java
@@ -0,0 +1,66 @@
+
+package acme.entities.activity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.enrolment.Enrolment;
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+// import acme.roles.Lecturer;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class Activity extends AbstractEntity {
+
+	//	Serialisation identifier ----------------------------
+	protected static final long	serialVersionUID	= 1L;
+
+	//	Attributes ----------------------------------------------
+
+	@NotBlank
+	@Length(min = 1, max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(min = 1, max = 100)
+	protected String			abstractt;
+
+	protected ActivityType		indication;
+
+	//Start y finish
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	protected Date				startPeriod;
+
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	protected Date				finishPeriod;
+
+	@NotBlank
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@Valid
+	@NotNull
+	@ManyToOne()
+	protected Enrolment			enrolment;
+
+}
