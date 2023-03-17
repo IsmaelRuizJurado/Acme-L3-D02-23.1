17abc4682393c60946507415310dc44055026bb6
PedroRuizAguilar <cocode2002@gmail.com>
2023-03-17 11:57:16 +0100

    Hotfix del code de tutorial

--- src/main/java/acme/entities/Tutorial.java
+++ src/main/java/acme/entities/Tutorial.java
@@ -30,7 +30,7 @@ public class Tutorial extends AbstractEntity {
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}[0-9]{3}$")
 	protected String			code;
 
 	@NotBlank
