69605de6dbfedcfc9875b0f12561e283ddc9eb67
PedroRuizAguilar <cocode2002@gmail.com>
2023-04-12 20:46:38 +0200

    Tarea 20 grupal done

---
+++ src/main/java/acme/features/administrator/configuration/ConfigurationController.java
@@ -0,0 +1,29 @@
+
+package acme.features.administrator.configuration;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.Configuration;
+import acme.framework.components.accounts.Administrator;
+import acme.framework.controllers.AbstractController;
+
+@Controller
+public class ConfigurationController extends AbstractController<Administrator, Configuration> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected ConfigurationShowService showService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("show", this.showService);
+	}
+
+}
